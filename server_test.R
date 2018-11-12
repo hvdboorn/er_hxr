2ca8fe49e2705827f47f926c218b0eb8500babce
Héctor van den Boorn <hvdboorn@gmail.com>
2018-11-12 18:24:40 +0100

    a

--- server_test.R
+++ server_test.R
@@ -21,3 +21,24 @@ client <- function(){
     close(con)
   }
 }
+#server()
+text = "The hexadecimal numeral system, also known as just hex, is a numeral system made up of 16 symbols (base 16). 
+The standard numeral system is called decimal (base 10) and uses ten symbols: 0,1,2,3,4,5,6,7,8,9. Hexadecimal uses the decimal numbers and includes six extra symbols. 
+There are no symbols that mean ten, or eleven etc. so these symbols are letters taken " %+% cyan("f") %+% "rom the English alphabet: A, B, C, D, E and F. 
+Hexadecimal A = decimal 10, and hexadecimal F = decimal 15.
+Humans mostly use the decimal system. This is probably because humans have ten fingers (ten digits). 
+Computers however, only have on and off, called a binary digit (or bit, for short). " %+% cyan("A") %+% " binary number is just a string of zeros and ones: 11011011, for example. 
+For convenience, engineers working with computers tend to group bits together. In earlier days, such as the 1960's, 
+they would group 3 bits at a time (much like large decimal numbers are grouped in threes, like the number 123,456,789). 
+Three bits, each being on or off, can represent the eight numbers from 0 to 7: 000 = 0; 001 = 1; 010 = 2; 011 = 3; 100 = 4; 101 = 5; 110 = 6 and 111 = 7. 
+This is called o"%+% cyan("c") %+% "tal. As computers got bigger, it was more convenient to group bits by four instead of three. 
+This doubles the numbers that the symbol would represent; it can h"%+% cyan("a") %+% "ve 16 values instead of eight. 
+Hex = 6 and Decimal = 10, so it is called hexa"%+% cyan("d") %+% "ecimal. 
+Four bits is called a nibble (sometimes spelled nybble). A nibble is one hexadecimal digit, and is written using a symbol 0-9 or A-F. 
+Two nibbles is a byte (8 bits). Most computer operations use the byte, or a multiple of the byte (16 bits, 24, 32, 64, etc.). 
+Hexadecimal makes it easier to write these large binary numbers. To avoid confusion with decimal, octal or other numbering systems, 
+hexadecimal numbers are sometim"%+% cyan("e") %+% "s written with a \"h\" after the number. For example, 63h means 63 hexadecimal. 
+Software developers quite often use 0x before the number (0x63)."
+
+cat(text)
+cat(strip_style(text))
